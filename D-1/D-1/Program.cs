a7a4794ab527043ca99fcaf54d9ef581984c42f8
DrayNeur <50057580+DrayNeur@users.noreply.github.com>
2020-05-02 00:55:31 +0200

    Added encryption alghorythm
    Added encryption alghorythm

---
+++ D-1/D-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace D_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\nEncrypted Text: " + EncryptionClass.Encrypt(RandomString(32),"Iorann est pas bo"));
+            Console.ReadKey();
+        }
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = @"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789/\@àçéè*-+=_#$£%ùµ§!:.;?,";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+    }
+}
